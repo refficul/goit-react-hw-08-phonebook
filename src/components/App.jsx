e24de4c339d8bb9868b319e777d441aa8d46f2f5
refficul <vic.w.toria.q@gmail.com>
2024-01-04 07:29:45 +0200

    main

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,67 @@
+import { GlobalStyle } from './GlobalStyle';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import { Layout } from './Layout/Layout';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, lazy } from 'react';
+import { refreshUser } from 'Redux/Authorization/operations'; // для обновления токена
+import { PrivateRoute } from './PrivateRoute'; // для захисту роутів
+import { RestrictedRoute } from './RestrictedRoute'; // для захисту роутів
+import { ToastContainer } from 'react-toastify'; // для вспливаючих повідомлень
+import { Spiner } from 'pages/ContactList/ContactList.styled';
+
+const Register = lazy(() => import('../pages/Register/Register'));
+const Home = lazy(() => import('../pages/Home/Home'));
+const Login = lazy(() => import('../pages/Login/Login'));
+const Contactlist = lazy(() => import('../pages/ContactList/ContactList'));
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(refreshUser()); // для обновления токена
+  }, [dispatch]);
+
+  const { isRefreshing } = useSelector(state => state.auth);
+
+  // якщо не обновляється токен, то рендеримо компоненти
+  return !isRefreshing ? (
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute component={<Contactlist />} redirectTo="/login" />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute redirectTo="/contacts" component={<Login />} />
+            }
+          />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<Register />}
+              />
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Route>
+      </Routes>
+
+      {/* для вспливаючих повідомлень */}
+      <ToastContainer />
+
+      {/* для глобальних стилів */}
+      <GlobalStyle />
+    </>
+  ) : (
+    <Spiner />
   );
 };
